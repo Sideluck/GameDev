70c9066ee46a4105d13e8bed1354c8f540d5fa0f
Sideluck <Kd1374947@st.kobedenshi.ac.jp>
2024-07-02 10:20:40 +0900

    7/2 進行

---
+++ 03_FPS/Src/Application/GameObject/Characters/Player/Player.cpp
@@ -0,0 +1,27 @@
+﻿#include "Player.h"
+#include "../../../GameObject/Camera/FPSCamera/FPSCamera.h"
+
+void Player::Update()
+{
+	if (GetAsyncKeyState('D')) { m_moveDir += {1,0,0}; }
+	if (GetAsyncKeyState('A')) { m_moveDir += {-1,0,0}; }
+	if (GetAsyncKeyState('W')) { m_moveDir += {0,0,1}; }
+	if (GetAsyncKeyState('S')) { m_moveDir += {0,0,-1}; }
+
+	m_pos = m_moveDir * m_moveSpeed;
+
+	const std::shared_ptr<CameraBase> _spCamera = m_wpCamera.lock();
+	if (_spCamera)
+	{
+		m_pos =
+			m_pos.TransformNormal(m_pos, _spCamera->GetRotationYMatrix());
+	}
+
+	Math::Matrix TransMat = Math::Matrix::CreateTranslation(m_pos);
+	m_mWorld = TransMat;
+}
+
+void Player::Init()
+{
+
+}
--- 03_FPS/Src/Application/Scene/GameScene/GameScene.cpp
+++ 03_FPS/Src/Application/Scene/GameScene/GameScene.cpp
@@ -1,6 +1,7 @@
 ﻿#include "GameScene.h"
 #include"../SceneManager.h"
 #include"../../main.h"
+#include "../../GameObject/Characters/Player/Player.h"
 #include "../../GameObject/Terrains/Terrain/Terrain.h"
 #include "../../GameObject/Camera/FPSCamera/FPSCamera.h"
 
@@ -19,9 +20,16 @@ void GameScene::Init()
 	//カーソル非表示
 	ShowCursor(false);
 
+	std::shared_ptr<Player> _player;
+	_player = std::make_shared<Player>();
+	_player->Init();
+	m_objList.push_back(_player);
+
 	std::shared_ptr<FPSCamera> _camera;
 	_camera = std::make_shared<FPSCamera>();
 	_camera->Init();
+	_camera->SetTarget(_player);
+	_player->SetCamera(_camera);
 	m_objList.push_back(_camera);
 
 	std::shared_ptr<Terrain> _terrain;
